667f21fdfef74cdc8490deec3d0d23304b252673
Ricardo Casanova <ricardo@pijp.eu>
2019-02-08 14:59:55 +0100

    Add RecipesListProtocols file

---
+++ iRecipe/iRecipe/Views/Recipes List/RecipesListProtocols.swift
@@ -0,0 +1,25 @@
+//
+//  RecipesListProtocols.swift
+//  iRecipe
+//
+//  Created by Ricardo Casanova on 08/02/2019.
+//  Copyright © 2019 Pijp. All rights reserved.
+//
+
+import Foundation
+
+// View / Presenter
+protocol RecipesListViewInjection : class {
+}
+
+protocol RecipesListPresenterDelegate : class {
+}
+
+// Presenter / Interactor
+
+protocol RecipesListInteractorDelegate : class {
+}
+
+// Presenter / Router
+protocol RecipesListRouterDelegate : class {
+}
