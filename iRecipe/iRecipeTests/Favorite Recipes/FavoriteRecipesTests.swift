b259e8e474a627aadd9aa26abcad849de7672d19
Ricardo Casanova <ricardo@pijp.eu>
2019-02-10 15:19:58 +0100

    Add testSaveFavoriteRecipes method to FavoriteRecipesTests file

--- iRecipe/iRecipeTests/Favorite Recipes/FavoriteRecipesTests.swift
+++ iRecipe/iRecipeTests/Favorite Recipes/FavoriteRecipesTests.swift
@@ -13,15 +13,23 @@ class FavoriteRecipesTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+        FavoriteRecipeManager.shared.deleteAll()
     }
 
     override func tearDown() {
         super.tearDown()
+        FavoriteRecipeManager.shared.deleteAll()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSaveFavoriteRecipes() {
+        FavoriteRecipeManager.shared.deleteAll()
+        
+        FavoriteRecipeManager.shared.save(title: "Baked Omelet With Broccoli &amp; Tomato", href: "http://www.recipezaar.com/Baked-Omelet-With-Broccoli-Tomato-325014", ingredients: "milk, cottage cheese, broccoli, cheddar cheese, basil, onion powder, eggs, garlic powder, roma tomato, salt", thumbnail: "http://img.recipepuppy.com/123889.jpg")
+        
+        FavoriteRecipeManager.shared.save(title: "Mild Curry Omelet", href: "http://allrecipes.com/Recipe/Mild-Curry-Omelet/Detail.aspx", ingredients: "coriander, cumin, eggs, garlic, green onion, vegetable oil, onions, red pepper, salt, turmeric", thumbnail: "")
+        
+        let favoriteRecipes = FavoriteRecipeManager.shared.getAll()
+        XCTAssert(favoriteRecipes.count == 2)
     }
 
     func testPerformanceExample() {
