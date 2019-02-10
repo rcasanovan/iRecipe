55c2418b62a92f731b37061ebb6f08723fc373a1
Ricardo Casanova <ricardo@pijp.eu>
2019-02-10 15:13:37 +0100

    Add initial FavoriteRecipesTests class

---
+++ iRecipe/iRecipeTests/Favorite Recipes/FavoriteRecipesTests.swift
@@ -0,0 +1,34 @@
+//
+//  FavoriteRecipesTests.swift
+//  iRecipeTests
+//
+//  Created by Ricardo Casanova on 10/02/2019.
+//  Copyright © 2019 Pijp. All rights reserved.
+//
+
+import XCTest
+@testable import iRecipe
+
+class FavoriteRecipesTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
