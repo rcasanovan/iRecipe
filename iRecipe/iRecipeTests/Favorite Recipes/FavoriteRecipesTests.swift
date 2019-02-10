741db415ef8059ebf90d24a03e39296564e753a2
Ricardo Casanova <ricardo@pijp.eu>
2019-02-10 15:25:18 +0100

    Update FavoriteRecipesTests with more unit testing

--- iRecipe/iRecipeTests/Favorite Recipes/FavoriteRecipesTests.swift
+++ iRecipe/iRecipeTests/Favorite Recipes/FavoriteRecipesTests.swift
@@ -31,12 +31,39 @@ class FavoriteRecipesTests: XCTestCase {
         let favoriteRecipes = FavoriteRecipeManager.shared.getAll()
         XCTAssert(favoriteRecipes.count == 2)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testDeleteAllFavoriteRecipes() {
+        FavoriteRecipeManager.shared.save(title: "Baked Omelet With Broccoli &amp; Tomato", href: "http://www.recipezaar.com/Baked-Omelet-With-Broccoli-Tomato-325014", ingredients: "milk, cottage cheese, broccoli, cheddar cheese, basil, onion powder, eggs, garlic powder, roma tomato, salt", thumbnail: "http://img.recipepuppy.com/123889.jpg")
+        
+        FavoriteRecipeManager.shared.save(title: "Mild Curry Omelet", href: "http://allrecipes.com/Recipe/Mild-Curry-Omelet/Detail.aspx", ingredients: "coriander, cumin, eggs, garlic, green onion, vegetable oil, onions, red pepper, salt, turmeric", thumbnail: "")
+        
+        FavoriteRecipeManager.shared.deleteAll()
+        
+        let favoriteRecipes = FavoriteRecipeManager.shared.getAll()
+        XCTAssert(favoriteRecipes.count == 0)
+    }
+    
+    func testFavoriteRecipesOrder() {
+        FavoriteRecipeManager.shared.deleteAll()
+        
+        FavoriteRecipeManager.shared.save(title: "Baked Omelet With Broccoli &amp; Tomato", href: "http://www.recipezaar.com/Baked-Omelet-With-Broccoli-Tomato-325014", ingredients: "milk, cottage cheese, broccoli, cheddar cheese, basil, onion powder, eggs, garlic powder, roma tomato, salt", thumbnail: "http://img.recipepuppy.com/123889.jpg")
+        
+        FavoriteRecipeManager.shared.save(title: "Mild Curry Omelet", href: "http://allrecipes.com/Recipe/Mild-Curry-Omelet/Detail.aspx", ingredients: "coriander, cumin, eggs, garlic, green onion, vegetable oil, onions, red pepper, salt, turmeric", thumbnail: "")
+        
+        let favoriteRecipes = FavoriteRecipeManager.shared.getAll()
+        XCTAssert(favoriteRecipes[0].title == "Baked Omelet With Broccoli &amp; Tomato" &&
+            favoriteRecipes[1].title == "Mild Curry Omelet")
+    }
+    
+    func testFavoriteRecipeExists() {
+        FavoriteRecipeManager.shared.deleteAll()
+        
+        FavoriteRecipeManager.shared.save(title: "Baked Omelet With Broccoli &amp; Tomato", href: "http://www.recipezaar.com/Baked-Omelet-With-Broccoli-Tomato-325014", ingredients: "milk, cottage cheese, broccoli, cheddar cheese, basil, onion powder, eggs, garlic powder, roma tomato, salt", thumbnail: "http://img.recipepuppy.com/123889.jpg")
+        
+        FavoriteRecipeManager.shared.save(title: "Mild Curry Omelet", href: "http://allrecipes.com/Recipe/Mild-Curry-Omelet/Detail.aspx", ingredients: "coriander, cumin, eggs, garlic, green onion, vegetable oil, onions, red pepper, salt, turmeric", thumbnail: "")
+        
+        let favoriteRecipeExists = FavoriteRecipeManager.shared.favoriteRecipeExists("Baked Omelet With Broccoli &amp; Tomato")
+        XCTAssert(favoriteRecipeExists == true, "The recipe doesn't exist in the data base")
     }
 
 }
