9e4c90ad4d296b12d3fcd977729641c1b53927d2
Ricardo Casanova <ricardo@pijp.eu>
2019-02-10 15:07:07 +0100

    Add initial FavoriteRecipeManager class

--- iRecipe/iRecipe/Managers/Favorite Recipe Manager/FavoriteRecipeManager.swift
+++ iRecipe/iRecipe/Managers/Favorite Recipe Manager/FavoriteRecipeManager.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import RealmSwift
+
+class FavoriteRecipeManager: NSObject {
+    
+    static let shared: FavoriteRecipeManager = { return FavoriteRecipeManager() }()
+    
+    public func save(id: String, title: String, href: String, ingredients: String, thumbnail: String) {
+        if favoriteRecipeExists(id) { return }
+        
+        let favoriteRecipe = FavoriteRecipe()
+        favoriteRecipe.recipeId = id
+        favoriteRecipe.title = title
+        favoriteRecipe.href = href
+        favoriteRecipe.ingredients = ingredients
+        favoriteRecipe.thumbnail = thumbnail
+        
+        // Get the default Realm
+        let realm = try! Realm()
+        
+        try! realm.write {
+            realm.add(favoriteRecipe)
+        }
+    }
+    
+    public func favoriteRecipeExists(_ id: String) -> Bool {
+        let realm = try! Realm()
+        let favoriteRecipes = realm.objects(FavoriteRecipe.self).filter("recipeId == %@", id)
+        return favoriteRecipes.count == 1
+    }
+    
+}
